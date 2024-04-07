8d0ddb2afc5fae1904c9b8b4986cd78caaebb8de
Monstertad <syong0729@gmail.com>
2024-04-07 16:43:05 +0900

    11718번 - 그대로 출력하기

---
+++ 202402686/11718.java
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        while (scanner.hadNextLine()){
+			String l = sc.nextLine();
+			System.out.println(l);
+		}
+        sc.close();
+		}
+		
+	}
+
+}
