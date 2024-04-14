012c8053566f96c9f205ac83c631909832eb1353
alpakaka0o0 <115137708+alpakaka0o0@users.noreply.github.com>
2024-04-14 22:42:26 +0900

    10825번 - 국영수

--- 202102655/10825.py
+++ 202102655/10825.py
@@ -7,4 +7,3 @@ table.sort(key = lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for student in table:
     sys.stdout.write(str(student[0]) + "\n")
-출처: https://suri78.tistory.com/109 [공부노트:티스토리]
