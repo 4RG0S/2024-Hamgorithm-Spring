ea4b24c42a516ac0488292a49e1b01abae04a64d
Jiu Song <jiusong@Jiuui-MacBookPro.local>
2024-04-14 14:00:17 +0900

    10825번 - 국영수

---
+++ 202102655/10825.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+table = [list(sys.stdin.readline().split()) for _ in range(N)]
+
+table.sort(key = lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in table:
+    sys.stdout.write(str(student[0]) + "\n")
+출처: https://suri78.tistory.com/109 [공부노트:티스토리]
