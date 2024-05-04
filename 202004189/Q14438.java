6133222ed8ba68bf37dde1c34ddb918d718139c9
0tae <choiyt3465@naver.com>
2024-05-05 05:21:05 +0900

    14438 - 수열과 쿼리 17

--- 202004189/Q14438.java
+++ 202004189/Q14438.java
@@ -2,9 +2,10 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.IntStream;
 
-public class Q18436 {
+public class Q14438 {
     static int[] src;
-    static Item[] segTree;
+    static int[] segTree;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -17,7 +18,7 @@ public class Q18436 {
         StringBuilder writer = new StringBuilder();
 
         src = new int[N+1];
-        segTree = new Item[N*4+1];
+        segTree = new int[N*4+1];
 
         for(int i=1;i<N+1;i++){
             src[i]=Integer.parseInt(s.nextToken());
@@ -35,8 +36,8 @@ public class Q18436 {
             if(cmd==1){
                 update(1,1,N,value1,value2);
             }else{
-                Item result = read(1,1,N,value1,value2);
-                writer.append(cmd==2 ? result.even : result.odd).append("\n");
+                int result = read(1,1,N,value1,value2);
+                writer.append(result).append("\n");
             }
         }
 
@@ -45,57 +46,38 @@ public class Q18436 {
         System.out.println(writer);
     }
 
-    public static Item segment(int nodeIdx, int start, int end){
+    public static int segment(int nodeIdx, int start, int end){
         if(start==end){
-            int odd = src[start] % 2==0 ? 0:1;
-            int even = src[start] % 2==0 ? 1:0;
-            return segTree[nodeIdx] = new Item(odd,even);
+            return segTree[nodeIdx] = src[start];
         }
 
         int mid = (start+end)/2;
         return segTree[nodeIdx] =
-                Item.sum(segment(nodeIdx*2,start,mid),segment(nodeIdx*2+1,mid+1,end));
+                Math.min(segment(nodeIdx*2,start,mid),segment(nodeIdx*2+1,mid+1,end));
     }
 
-    public static Item read(int nodeIdx, int start, int end, int targetStart, int targetEnd){
+    public static int read(int nodeIdx, int start, int end, int targetStart, int targetEnd){
         if(targetEnd < start || targetStart > end){
-            return new Item(0,0);
+            return Integer.MAX_VALUE;
         }else if(start >=targetStart && end <= targetEnd) {
             return segTree[nodeIdx];
         }
         int mid = (start+end)/2;
-        return Item.sum(read(nodeIdx*2,start,mid,targetStart,targetEnd),
+        return Math.min(read(nodeIdx*2,start,mid,targetStart,targetEnd),
                 read(nodeIdx*2+1,mid+1,end,targetStart,targetEnd));
     }
 
-    public static Item update(int nodeIdx, int start, int end, int updatedIdx, int updateValue){
+    public static int update(int nodeIdx, int start, int end, int updatedIdx, int updateValue){
         if (!(updatedIdx < start || updatedIdx > end)) {
             if(start==end){
-                int odd = updateValue % 2==0 ? 0:1;
-                int even = updateValue % 2==0 ? 1:0;
-                return segTree[nodeIdx] = new Item(odd,even);
+                return segTree[nodeIdx] = updateValue;
             }
 
             int mid = (start+end)/2;
-            return segTree[nodeIdx] =
-                    Item.sum(update(nodeIdx*2,start,mid,updatedIdx,updateValue),
+            return segTree[nodeIdx] = Math.min(update(nodeIdx*2,start,mid,updatedIdx,updateValue),
                     update(nodeIdx*2+1,mid+1,end,updatedIdx,updateValue));
         }
 
         return segTree[nodeIdx];
-    }
-}
-
-class Item{
-    int value;
-    int odd;
-    int even;
-    Item(int o, int e){
-        odd=o;
-        even=e;
-    }
-
-    public static Item sum(Item o1, Item o2){
-        return new Item(o1.odd+o2.odd,o1.even+ o2.even);
     }
 }
