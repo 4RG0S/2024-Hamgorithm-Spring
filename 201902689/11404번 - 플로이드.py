85a9325d1c12a8149a4b86f3b4dc6fc77745a62d
MoonGoon <moongoon.cnu@gmail.com>
2024-05-05 23:14:58 +0900

    ✅ Solve: 11404번 - 플로이드.py
    comment:
    입력을 주의깊게 살펴보자.

---
+++ 201902689/11404번 - 플로이드.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
