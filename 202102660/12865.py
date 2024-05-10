bb426d75fda3ab297a28ba78d777f858df44c908
mininiii <didals0521@gmail.com>
2024-05-11 00:29:11 +0900

    12865번-평범한 배낭

--- 202102660/12865.py
+++ 202102660/12865.py
@@ -1,12 +1,15 @@
-
+# 다시 풀기
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
 items = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+items.sort(key=lambda x: x[0])
 
-dp = [0] * (N)
+dp = [0] * (K+1) # i무게까지 담을 수 있는 최대 가치
 
 for i in range(N):
-    dp[i] = items[i][1]
-    for j in range(N):
-        
+    for j in range(K, 0, -1):
+        if items[i][0] <= j:
+            dp[j] = max(dp[j], dp[j-items[i][0]] + items[i][1])
+
+print(dp[K])
