d147088002f03819e3c4065c025a9a58bb49e7bc
ddeonseo <eunseo1370@gmail.com>
2024-05-26 21:25:31 +0900

    1655번 가운데를 말해요

---
+++ 202102649/1655-가운데를말해요.py
@@ -0,0 +1,23 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+
+leftHeap = []
+rightHeap = []
+for i in range(n):
+    num = int(sys.stdin.readline())
+
+    if len(leftHeap) == len(rightHeap):
+        heapq.heappush(leftHeap, -num)
+    else:
+        heapq.heappush(rightHeap, num)
+
+    if rightHeap and rightHeap[0] < -leftHeap[0]:
+        leftValue = heapq.heappop(leftHeap)
+        rightValue = heapq.heappop(rightHeap)
+
+        heapq.heappush(leftHeap, -rightValue)
+        heapq.heappush(rightHeap, -leftValue)
+
+    print(-leftHeap[0])
