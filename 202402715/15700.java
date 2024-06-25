e90c9a44396d772868fab7193e283c341a67faee
becky5432 <165632128+becky5432@users.noreply.github.com>
2024-06-26 01:33:53 +0900

    15700번 - 타일 채우기 4

---
+++ 202402715/15700.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.print(a*b/2);
+    }
+}
