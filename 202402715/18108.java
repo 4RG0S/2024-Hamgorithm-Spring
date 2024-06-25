8ad89a11bb53542c78e08f1c0abb058620833697
becky5432 <165632128+becky5432@users.noreply.github.com>
2024-06-26 01:34:28 +0900

    18108번 - 1998년생인 내가 태국에서는 2541년생?!

---
+++ 202402715/18108.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int in = sc.nextInt();
+        System.out.print(in-543);
+    }
+}
